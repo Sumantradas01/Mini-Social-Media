20b40bb3e22849ec3dc9b47004cc5372faeef514
kanchan dutta <143182918+Kanchan200410@users.noreply.github.com>
2026-05-27 20:35:27 +0530

    kanchan

--- run.py
+++ run.py
@@ -1,6 +1,37 @@
-from app import create_app
+from flask import Flask
+from supabase import create_client
+from dotenv import load_dotenv
+import os
 
-app = create_app()
+# Load .env file
+load_dotenv()
+
+app = Flask(__name__)
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+print("URL =", SUPABASE_URL)
+print("KEY =", SUPABASE_KEY)
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    data = {
+        "full_name": "Test User",
+        "username": "testuser",
+        "email": "test@example.com",
+        "phone": "9876543210",
+        "password": "12345678"
+    }
+
+    response = supabase.table("users").insert(data).execute()
+
+    return str(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
